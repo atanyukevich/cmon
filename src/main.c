091c3659bd967d67f0690d815e9525eb980300fb
Alexander Tanyukevich <spam@tanyukevich.net>
2026-06-28 13:42:53 +0200

    printing version macros

--- src/main.c
+++ src/main.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 
+#include "cmon_config.h"
+
 /*****************************************************************************/
 int main (int argc, char ** argv)
 {
-
+	printf("CMon Version %d.%d\n", CMON_VERSION_MAJOR, CMON_VERSION_MINOR);
 	return 0;
 }
 /*****************************************************************************/
